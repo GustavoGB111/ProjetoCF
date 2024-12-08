6fa1395eb252f11761eb6ea165eb0bd0c26b8e4c
GustavoGB111 <gbgustavo172@gmail.com>
2024-12-08 14:19:48 -0300

    att pequena

---
+++ Cadastro/Cadastro.js
@@ -0,0 +1,5 @@
+const enviarForm = document.querySelector("#Button-Cadastro")
+
+enviarForm.addEventListener('click', function() {
+    alert('botão clicado')
+});
--- PagPrincipal/pagTodos.js
+++ PagPrincipal/pagTodos.js
@@ -1,6 +1,5 @@
     //Função Active para a permanencia de estado de um botão ativo//desativo
     const buttons = document.querySelectorAll('.Main-Div-Button');
-    const modalButtons = document.querySelectorAll('Modal-Button-Types');
     const typeBox = document.querySelectorAll('Main-Div-Tipo-Valor');
     const BoxesInput = document.querySelectorAll('Main-Div-Valor-Box');
 
@@ -13,14 +12,6 @@
                       button.classList.toggle('active'); 
                     }); });
 
-    modalButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            modalButtons.forEach(btn => {
-                if (btn !== button && btn.classList.contains('active')) {
-                    btn.classList.remove('active'); }
-                        });
-                    button.classList.toggle('active'); 
-                }); });
 
     //Função para modal
     const buttonOpenModal = document.querySelector("#Header-Title-Form-New_Value_Button")
@@ -34,18 +25,3 @@
         modal.close()
     }
 
-    // Pegar Elementos do formulario (Teste)
-    document.getElementById('Modal-Div-Form').addEventListener('submit', function(event) {
-        event.preventDefault();
-      
-        const valor = document.getElementById('Modal-Div-Form-Input').value;
-        const tipoEntrada = document.getElementById('Modal-Button-Entrada').value;
-        const tipoSaída = document.getElementById('Modal-Button-Saída').value;
-        const Transaction = {
-          Value: valor,
-          Type: tipoEntrada,
-          Type_2: tipoSaída
-        };
-      window.alert(Transaction);
-      });
-      
