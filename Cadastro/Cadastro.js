fb555dd2532fbd4cc67039a5802a059da8315065
GustavoGB111 <gbgustavo172@gmail.com>
2024-12-08 15:30:21 -0300

    att

--- Cadastro/Cadastro.js
+++ Cadastro/Cadastro.js
@@ -1,5 +1,17 @@
-const enviarForm = document.querySelector("#Button-Cadastro")
+document.querySelector('#Form-Login').addEventListener('submit', async function(event){
+   event.preventDefault();
 
-enviarForm.addEventListener('click', function() {
-    alert('botão clicado')
-});
+        const formData = new FormData(this);
+        if (formData.get('Confirm-Senha-Input') == null || formData.get('Senha-Input') != null ||formData.get('Confirm-Senha-Input') != formData.get('Senha-Input')) {
+        
+        } else {
+            const filteredData = {
+                name: formData.get('Nome-Input'),
+                email: formData.get('Email-Input'),
+                password: formData.get('Senha-Input'),
+                confirm_password: formData.get('Confirm-Senha-Input')
+            };
+    }
+    } 
+);
+
